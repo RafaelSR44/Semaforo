7e24e9b73ad6630f96d18a8a78325804d5a1c6cf
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2025-10-28 15:25:47 -0300

    fix: comentarios

--- src/semaforo.ino
+++ src/semaforo.ino
@@ -1,17 +1,20 @@
 // Código para semaforo em ESP32
 
+// Definição dos pinos dos LEDs
 #define LED_RED 4
 #define LED_YELLOW 2
 #define LED_GREEN 15
 
-class Semaforo {
+class Semaforo { // Classe Semaforo
   private:
+    // Variáveis privadas
     unsigned long previousMillis = 0;
     int intervalo = 1000;
     int8_t state = 0;
 
   public:
-
+    // Método para controlar o tempo de atraso sem bloquear o loop principal
+    // Retorna true quando o intervalo de tempo especificado tiver passado
   bool delayMillis(int interval) {
     intervalo = interval;
     unsigned long currentMillis = millis();
@@ -22,7 +25,7 @@ class Semaforo {
       return false;
     }
   }
-
+  // Métodos para controlar os LEDs
   void red(){
     digitalWrite(LED_RED, HIGH);
     digitalWrite(LED_YELLOW, LOW);
@@ -40,7 +43,9 @@ class Semaforo {
     digitalWrite(LED_YELLOW, LOW);
     digitalWrite(LED_GREEN, HIGH);
   }
-
+  // Método principal para controlar o semáforo
+  // Alterna entre os estados do semáforo com base no tempo definido
+  // Estado 0: Vermelho, Estado 1: Verde, Estado 2: Amarelo
   void working(){
     while (!delayMillis(6000) && state == 0){
       red();
@@ -65,8 +70,10 @@ class Semaforo {
 
 };
 
+// Instância da classe Semaforo
 Semaforo semaforo;
 
+// Função de configuração
 void setup()  {
   Serial.begin(115200);
   pinMode(LED_RED, OUTPUT);
@@ -75,6 +82,7 @@ void setup()  {
 
 }
 
+// Função principal de loop
 void loop()  {
   semaforo.working();
 }
